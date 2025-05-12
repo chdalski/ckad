6e27e1b4057a674d88be8b7faa14492205671cd2
Christoph Dalski <chdalski.coding@gmail.com>
2025-05-12 11:12:24 +0200

    Add env.sh and update README

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# ckad
+# CKAD Exam Resources
+
+## Setup environment
+
+`source <(curl "https://raw.githubusercontent.com/chdalski/ckad/refs/heads/main/env.sh")`
---
+++ env.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# used to list files in the current directory
+alias l='ls -lisa'
+
+# used in kubectl create / run commands with $dry (i. e. kubectl create ... $dry > resource.yml
+alias dry='--dry-run=client -o yaml'
+
+# destroy resources instandly (i. e. kubectl delete -f resource.yml $now)
+alias now='--grace-period 0 --force'
+
+# set default namespace
+sdn() { kubectl config set-context --current --namespace="$1" ; }
+
+# reset default namespace
+rdn() { kubectl config set-context --current --namespace=default ; }
+
+# organize files per question
+mkcd() { mkdir -p "$@" && cd "$@" ; }
+
+# create/destroy from yaml faster
+alias kaf='k apply -f'
+alias kdf='k delete -f'
